87b2e6bac14f0747be0d2254be073ad7ce6b32e7
Stanislav Aleksandrov <lightofmysoul@gmail.com>
2026-01-15 21:20:55 +0000

    Add properties to surface and subcompositor needed for VR mode
    SurfaceInterface: add subSurface property
    SubSurfaceInterface: add position property

--- src/wayland/surface.h
+++ src/wayland/surface.h
@@ -91,6 +91,7 @@ class KWIN_EXPORT SurfaceInterface : public QObject
      */
     Q_PROPERTY(QRegion input READ input NOTIFY inputChanged)
     Q_PROPERTY(QSizeF size READ size NOTIFY sizeChanged)
+    Q_PROPERTY(SubSurfaceInterface *subSurface READ subSurface CONSTANT)
 public:
     explicit SurfaceInterface(CompositorInterface *compositor, wl_resource *resource);
     ~SurfaceInterface() override;
